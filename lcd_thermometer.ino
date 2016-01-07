b69fb85e1ee19fbb7893a92ba0036dfcf0483bc2
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2016-01-07 18:40:47 +0100

    refactored (untested)

--- lcd_thermometer.ino
+++ lcd_thermometer.ino
@@ -2,66 +2,92 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-const int BACK_LIGHT = 13;
-const int TEMP = 0;
-const int BUTTON = 7;
+const int BACK_LIGHT_PIN = 13;
+const int TEMP_PIN = 0;
+const int BUTTON_PIN = 7;
 
 const int TIME_ON = 10000;
+
+typedef struct {
+  float temp;
+  float humidity;
+} Temp;
 
 void setup() {
   Serial.begin(9600);
   
-  pinMode(BACK_LIGHT, OUTPUT);
-  pinMode(BUTTON, INPUT);
-  digitalWrite(BACK_LIGHT, HIGH);
-
+  pinMode(BACK_LIGHT_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  
   lcd.begin(16,2);
   lcd.clear();
   lcd.setCursor( 0, 0 );
-  lcd.print( "--- INIT ---");
-  
+  lcd.print( "Thermometer ...");
+
+  displayOn();
   delay(2000);
-  
-  lcd.noDisplay();
-  digitalWrite(BACK_LIGHT, LOW);
+  displayOff();
 }
 
 void loop() {
 
-  int buttonState = digitalRead(BUTTON);
+  int buttonState = digitalRead(BUTTON_PIN);
   
   if ( buttonState == HIGH) {
-    lcd.display();
-    digitalWrite(BACK_LIGHT, HIGH);
-    lcd.setCursor( 0, 0 );
-    lcd.print( "Temperatura:");
+    Temp t = readTemp();
+  
+    lcdWrite(t);
+    serialWrite(t);
     
-    int val_Adc = analogRead(TEMP);
-    //Converte il valore letto in tensione
-    float voltage = (val_Adc /1024.0) * 5.0;
-    //Converte il valore di tensione in temperatura
-    float temp = (voltage - .5) * 100;
-    // Trasmette sulla seriale il valore della tensione letto
-    Serial.print(voltage,4); 
-    Serial.print(" volts    ");
-    // Trasmette sulla seriale il valore della temperatura
-    Serial.print(temp,2);  
-    Serial.println(" gradi celsius");
-
-    lcd.setCursor( 3, 1 );
-    // scrive la temperatura
-    lcd.print( temp, 1 );
-    // lascia uno spazio
-    lcd.print( ' ' );
-    // Scrive '°'
-    lcd.print( (char) 223 );
-    //Scrive il carattere "C"
-    lcd.print( 'C' );
-
+    displayOn();
+    
     delay(TIME_ON);
   } else {
-    lcd.noDisplay();
-    digitalWrite(BACK_LIGHT, LOW);
-    lcd.clear();
+    displayOff();
   }
 }
+
+Temp readTemp() {
+  int val_Adc = analogRead(TEMP_PIN);
+  float voltage = (val_Adc /1024.0) * 5.0;
+  //float temp = ;
+  
+  return Temp { (voltage - .5) * 100, 0 };
+}
+
+void lcdWrite(Temp t) {
+    lcd.clear();
+    
+    // Temperature
+    lcd.setCursor( 0, 0 );
+    lcd.print("Temp.: ");
+    lcd.print( t.temp, 1 );
+    lcd.print( ' ' );
+    lcd.print( (char) 223 );
+    lcd.print( 'C' );
+
+    // Humidity
+    lcd.setCursor( 0, 0 );
+    lcd.print("Hum.: ");
+    lcd.print( t.humidity, 1 );
+    lcd.print( '%' );
+}
+
+void serialWrite(Temp t) {
+  Serial.print("Temperature: ");
+  Serial.print(t.temp, 2);
+  Serial.print("        Humidity:");
+  Serial.println(t.humidity, 0);
+}
+
+void displayOn() {
+    lcd.display();
+    digitalWrite(BACK_LIGHT_PIN, HIGH);
+}
+
+void displayOff() {
+    lcd.noDisplay();
+    digitalWrite(BACK_LIGHT_PIN, LOW);
+}
+
+
